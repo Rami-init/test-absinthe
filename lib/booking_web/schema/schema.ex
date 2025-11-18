529239d7f38f6a04aa36879d38657f72b7c7afe5
Rami Sami <ramisamidev@gmail.com>
2025-11-18 17:42:02 +0200

    feat(schema): [FAR] add graphql frist shema

--- lib/booking_web/schema/schema.ex
+++ lib/booking_web/schema/schema.ex
@@ -1,6 +1,9 @@
 defmodule BookingWeb.Schema.Schema do
   use Absinthe.Schema
-
+  #  doc """
+  #  The main schema for the BookingWeb application.
+  #  It defines the GraphQL types, queries, and mutations.
+  #  """
   query do
     field :hello, :string do
       resolve(fn _, _ -> {:ok, "Hello, world!"} end)
